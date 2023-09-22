11494ca81c19e15ceb88bcca265923704405b03f
quanmbn <144711880+quanmbn@users.noreply.github.com>
2023-09-22 15:46:04 +0000

    classroom work

---
+++ introPyrhonActivities/dataCasting.py
@@ -0,0 +1,22 @@
+# convert the following datatypes into floats.
+12
+100
+float(12)
+float(100)
+# convert the following floats into integers.
+12.34
+23.33
+int(12.34)
+int(23.33)
+ 
+# modify the code below to print an integer number. 
+num_one = float(20)
+num_two= 23.45
+
+print(num_one + num_two)
+
+# modify the following code below to 
+name_of_pet = "this is my dog spuny, he is"
+age_of_pet = "10 years old" 
+
+print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
